6fe05cccbef7b4658770e75630395d42142c1a21
rafael2000-prog <rafsantiserrano@hotmail.com>
2025-11-26 17:58:07 -0500

    completadoPrueba

--- NodoC/nodoC.js
+++ NodoC/nodoC.js
@@ -3,6 +3,17 @@ const app = express();
 
 app.use(express.json());
 
+// CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 
 app.post('/verificar', async (req, res) => {
   try {
